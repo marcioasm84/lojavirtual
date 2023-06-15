5cad4112ae958084b14fd997a2c4cd67affeeaee
Lucas <casmarques03@gmail.com>
2023-06-15 16:15:06 -0300

    Cadastro-Marcas

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("salvarButton").addEventListener("click", function() {
+    var nome = document.getElementById("nome").value;
+    
+    if (nome === "") {
+      alert("Informe o Nome");
+    } else {
+      alert("Tipo de marca salvo com sucesso");
+    }
+  });
